5efcec6ca16f70e241840ecc9f3bf6bd0402e1bb
Jorge Felico <jorgefelico@gmail.com>
2023-02-11 23:43:07 -0500

    Started on calendar

--- src/calendar.ts
+++ src/calendar.ts
@@ -3,6 +3,7 @@ import { ICalendarOptions } from "./calendar.interface";
 class DateDreamerCalendar implements ICalendarOptions {
     element: HTMLElement | string;
     selectedDate: string | Date | null;
+    calendarElement: HTMLElement | null = null;
 
     constructor(options: ICalendarOptions) {
         console.log(options);
@@ -36,16 +37,8 @@ class DateDreamerCalendar implements ICalendarOptions {
             case "undefined":
                     break;
         }
-    }
 
-    /**
-     * 
-     * @returns {string} The HTML for the calendar element.
-     */
-    private renderCalendar():string {
-        return `<div class="datedreamer__calendar">
-            Calendar goes here :)
-        </div>`
+        this.generateDays();
     }
 
     /**
@@ -56,9 +49,70 @@ class DateDreamerCalendar implements ICalendarOptions {
         const selectedElement = document.querySelector(this.element as string);
         if(selectedElement) {
             selectedElement.innerHTML = calendar;
+            this.calendarElement = selectedElement.querySelector(".datedreamer__calendar");
         } else {
             console.error(`Could not find ${this.element} in DOM.`);
         }
+    }
+
+    /**
+     * 
+     * @returns {string} The HTML for the calendar element.
+     */
+    private renderCalendar():string {
+        return `<div class="datedreamer__calendar">
+            <div class="datedreamer__calendar_header">
+                <button class="datedreamer__calendar_prev" aria-label="Previous">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </button>
+                <span class="datedreamer__calendar_title">January 2022</span>
+                <button class="datedreamer__calendar_next" aria-label="Next">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>
+
+            <div class="datedreamer__calendar_days">
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">Mo</div>
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">Tu</div>
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">We</div>
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">Th</div>
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">Fr</div>
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">Sat</div>
+                <div class="datedreamer__calendar_day datedreamer__calendar_day-header">Su</div>
+            </div>
+        </div>`
+    }
+
+    private generateDays():string {
+        const daysElementContainer:HTMLElement | null | undefined = this.calendarElement?.querySelector(".datedreamer__calendar_days");
+        
+        let offset = 0;
+        
+        const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+        const today = new Date();
+        const month = today.getMonth();
+        const day = today.getDate();
+        const year = today.getFullYear();
+        const daysInMonth = new Date(year, month + 1,0).getDate();
+        const firstDayOfMonth = new Date(year,month,1);
+        const daysToSkip = weekdays.indexOf(firstDayOfMonth.toString().split(" ")[0]);
+        
+        for(let i = 1; i <= daysToSkip + daysInMonth; i++) {
+            if(i > daysToSkip) {
+                const day = document.createElement("div");
+                day.classList.add("datedreamer__calendar_day");
+                day.innerText = (i - daysToSkip).toString();
+                daysElementContainer?.append(day);
+            } else {
+                const day = document.createElement("div");
+                day.classList.add("datedreamer__calendar_day");
+                day.innerText = "0";
+                daysElementContainer?.append(day);
+            }
+        }
+
+        return "";
     }
 }
 
