2925b4ecfad81f4ab82fd5b7d39648f44809d778
Jorge Felico <jorgefelico@gmail.com>
2023-02-17 00:52:08 -0500

    fix: Added white background to calendar

--- src/utils.ts
+++ src/utils.ts
@@ -52,6 +52,7 @@ export const unstyledTheme = `
     max-width: 220px;
     padding: 14px;
     box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+    background: #fff;
 }
 
 .datedreamer__calendar_header {
